f77558b90eeb49cb971730b78c9f12405c82fc57
Takahashi <ne300229@senshu-u.jp>
2020-10-26 23:58:47 +0900

    いらんdoを訂正

--- ed.rb
+++ ed.rb
@@ -1,19 +1,25 @@
 class REPL
-    def initialize do
+    def initialize
         loop{
             read
             eval
             print
         }
     end
+    @buffer = [] # バッファ
+    @
 
     def read # コマンド受け付け
+        STDIN.gets
+        puts($_)
     end
 
     def eval # 解釈
+        puts($_)
     end
     
     def print # 結果を出力
+        puts("print")
     end
 end
 
