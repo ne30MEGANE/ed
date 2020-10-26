265639adaab39b499dd14916d54512ac634d426a
Takahashi <ne300229@senshu-u.jp>
2020-10-27 01:44:43 +0900

    ver.0

--- ed.rb
+++ ed.rb
@@ -1,25 +1,60 @@
 class REPL
     def initialize
+        @buffer = Array.new # バッファ
+        @input = Array.new # 毎回の入力用
+        @output = Array.new # 毎回の出力用
+        @prompt = "command>" # プロンプト
+        if ARGV[0] # ファイルが指定された時
+            f = open(ARGV[0])
+            f.each do |line|
+                @buffer.push(line.chomp) # 改行コードを消してバッファーに読み込む
+            end
+            puts("-- #{ARGV[0]} opend --")
+            puts(@buffer)
+        end
         loop{
-            read
-            eval
-            print
+            ed_read
+            ed_eval
+            ed_print
         }
     end
-    @buffer = [] # バッファ
-    @
 
-    def read # コマンド受け付け
-        STDIN.gets
-        puts($_)
+    def ed_read # コマンド受け付け
+        print(@prompt) # プロンプト出力
+        @input = STDIN.gets(chomp: true).split(" ")
     end
 
-    def eval # 解釈
-        puts($_)
+    def ed_eval # 解釈
+        case @input[0]
+        when "a" then
+            @output = "a"
+        when "c" then
+            @output = "c"
+        when "d" then
+            @output = "d"
+        when "g" then
+            @output = "g"
+        when "i" then
+            @output = "i"
+        when "j" then
+            @output = "j"
+        when "n" then
+            @output = "n"
+        when "q" then
+            @output = "q"
+        when "w" then
+            @output = "w"
+        when "wq" then
+            @output = "wq"
+        when "=" then
+            @output = "="
+        else
+            @output = "?" #どのコマンドにも当てはまらない時
+        end
     end
     
-    def print # 結果を出力
-        puts("print")
+    def ed_print # 結果を出力
+        puts(@output)
     end
 end
 
